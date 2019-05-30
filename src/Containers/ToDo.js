d56e50e41af437c357a52971f185b8a1c93481cb
vitaminvp <vitamin@ukr.net>
2019-05-30 10:20:12 +0300

    Add disabled for button

--- src/Containers/ToDo.js
+++ src/Containers/ToDo.js
@@ -132,7 +132,7 @@ class ToDo extends Component {
                 />
                 {isTasksExist && (
                   <List
-                    currentTaskId={currentTaskId+''}
+                    currentTaskId={currentTaskId.toString()}
                     tasksList={filteredTasks}
                     removeTask={removeTask}
                     completeTask={completeTask}
